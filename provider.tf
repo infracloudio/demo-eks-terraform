ca2b1c07b594e7daff80787e89efc261ebb2f322
Rahul Kadam <rkadam@infracloud.io>
2020-08-05 20:42:18 +0530

    Region change as we want the reosurce to be in Mumbai region and added gitignore to avoid adding terraform packges

--- provider.tf
+++ provider.tf
@@ -2,17 +2,17 @@
 # Note: Change the region name as per requirement
 
 provider "aws" {
- region  = "us-east-1"  
+ region  = "ap-south-1"  
 }
 
 
 # Additionally we can have backend block in here, if we want to manage the terraform state file in cloud (for eg AWS s3 as shown in example below, uncomment it if needed)
 
 
-# terraform {
-#   backend "s3" {
-#     bucket = "mybucket"
-#     key    = "path/to/my/key"
-#     region = "us-east-1"
-#   }
-# }
+terraform {
+  backend "s3" {
+    bucket = "eks-test-rahulkadam"
+    key    = "demo1/terraform.tfstate"
+    region = "ap-south-1"
+  }
+}
