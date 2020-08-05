80bfd0e278aff11b0e1c1ec9a0ce5e13854ecc62
Rahul Kadam <rkadam@infracloud.io>
2020-08-05 21:00:28 +0530

    Created variable file to deal with terraform errors

---
+++ variables.tf
@@ -0,0 +1,55 @@
+variable "vpc_cidr_block" {
+  type        = string
+  default     = "10.210.0.0/16"
+  description = "VPC under which cluster needs to be created"
+}
+
+variable "subnet1" {
+  type        = map
+  description = "CIDR and AZ for Subnet1"
+}
+
+variable "subnet2" {
+  type        = map
+  description = "CIDR and AZ for Subnet2"
+}
+
+variable "subnet3" {
+  type        = map
+  description = "CIDR and AZ for Subnet3"
+}
+
+variable "subnet4" {
+  type        = map
+  description = "CIDR and AZ for Subnet4"
+}
+
+variable "eks_node_desired_size" {
+  type        = number
+  description = "Desired size of cluster"
+}
+
+variable "eks_node_min_size" {
+  type        = number
+  description = "Minimum number of nodes in cluster"
+}
+
+variable "eks_node_max_size" {
+  type        = number
+  description = "Maximum number of nodes in cluster"
+}
+
+variable "eks_node_instance_types" {
+  type        = string
+  description = "Instance type of nodes in cluster"
+}
+
+variable "node_ami_type" {
+  type        = string
+  description = "AMI type of cluster nodes"
+}
+
+variable "k8s_version" {
+  type        = string
+  description = "Kubernetes version on the cluster"
+}
