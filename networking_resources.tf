a5410773804e0eb766e06dde3f32e6c21e9cec8a
Rahul Kadam <rkadam@infracloud.io>
2020-08-06 13:43:58 +0530

    Fixed issue with NAT gateway internet access

--- networking_resources.tf
+++ networking_resources.tf
@@ -58,7 +58,7 @@ resource "aws_eip" "nat-eip" {
 
 resource "aws_nat_gateway" "nat-gw" {
   allocation_id = aws_eip.nat-eip.id
-  subnet_id = aws_subnet.eks_subnet_1.id
+  subnet_id = aws_subnet.eks_subnet_3.id
 }
 
 # Internet gateway for public subnets
